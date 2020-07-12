8ed1360a1816e76e781aeeb3120aad9a07f17bb3
Greg Dick <gregdick1@gmail.com>
2020-07-12 13:53:40 -0500

    Set up hello world version of the bolt app.

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const { App } = require('@slack/bolt');
+const store = require('./store');
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN
+});
+
+
+app.event('app_home_opened', ({ event, say }) => {  
+  // ignore if not the home tab
+  if(event.tab !== 'home') {
+    return;
+  }
+  // Look up the user from DB
+  let user = store.getUser(event.user);
+  console.log(event);
+  
+  if(!user) {
+    user = {
+      user: event.user,
+      channel: event.channel
+    };
+    store.addUser(user);
+    
+    say(`Hello world, and welcome <@${event.user}>!`);
+  } else {
+    say('Hi again!');
+  }
+});
+
+
+// Start your app
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
+})();
+
