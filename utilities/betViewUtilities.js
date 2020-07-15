1b778aa58491d1aacdd1b830c4f7aa5ea0b9724a
Greg Dick <gregdick1@gmail.com>
2020-07-14 19:23:40 -0500

    Make bet post smaller.

--- utilities/betViewUtilities.js
+++ utilities/betViewUtilities.js
@@ -4,14 +4,24 @@ exports.getBetPostView = (bet, statusDisplay, pointsRemaining) => {
   const blocks = [
     blockKitUtilities.markdownSection(`<@${bet.slackId}> wants to make a bet!`),
     blockKitUtilities.divider(),
-    blockKitUtilities.markdownSection(bet.scenarioText),
-    blockKitUtilities.divider()
   ]
+  
   if (statusDisplay === 'Open') {
-    blocks.push(blockKitUtilities.markdownSectionWithAccessoryButton(`*Status:* Open\n *Amount:* ${bet.pointsBet} pts\n*Remaining:* ${pointsRemaining} pts`, "Accept Bet", "accept_bet"));
+    blocks.push(blockKitUtilities.markdownSectionWithAccessoryButton(bet.scenarioText, "Accept Bet", "accept_bet"));
   } else {
-    blocks.push(blockKitUtilities.markdownSection(`*Status:* ${statusDisplay}\n *Amount:* ${bet.pointsBet} pts\n*Remaining:* ${pointsRemaining} pts`));
+    blocks.push(blockKitUtilities.markdownSection(bet.scenarioText));
   }
+  blocks.push(blockKitUtilities.divider());
+
+  blocks.push({
+    type: 'context',
+    elements: [
+      {
+        type: "mrkdwn",
+        text: `*Status:* ${statusDisplay}  *Amount:* ${bet.pointsBet} pts  *Remaining:* ${pointsRemaining} pts`,
+      },
+    ]
+  });
   blocks.push(blockKitUtilities.buttonAction("bet_actions", "Submit Results", "submit_results_from_channel"));
   return blocks;
 }
